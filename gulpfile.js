9441620b187effd281312d6821d2ebbab6969f9e
Gerardo Cignarella <cignarella.gerardo@gmail.com>
2016-08-24 17:59:39 -0400

    deleted coffee script

--- gulpfile.js
+++ gulpfile.js
@@ -23,20 +23,19 @@ elixir(function(mix) {
     .sass([
         "app.scss"
     ], "public/assets/css/app.css")
+    .scripts([
+        'app.js'
+    ], "public/assets/js/app.js")
+    .scripts([
+        'page.js'
+    ], "public/assets/js/page.js")
     .copy([
         componentsPath + 'html5shiv/dist/html5shiv.min.js',
         componentsPath + 'respond/dest/respond.min.js',
         componentsPath + 'jquery/dist/jquery.min.js',
-        componentsPath + 'bootstrap-sass/assets/javascripts/bootstrap.min.js',
-        'resources/assets/js'
+        componentsPath + 'bootstrap-sass/assets/javascripts/bootstrap.min.js'
         ], 'public/assets/js')
     .copy(componentsPath + 'bootstrap-sass/assets/fonts', 'public/assets/fonts')
     .copy(componentsPath + 'font-awesome/fonts', 'public/assets/fonts/font-awesome')
-    .copy('resources/assets/fonts', 'public/assets/fonts')
-    .coffee([
-        'app.coffee'
-        ], 'public/assets/js/app.js' )
-    .coffee([
-        'page.coffee'
-        ], 'public/assets/js/page.js' );
+    .copy('resources/assets/fonts', 'public/assets/fonts');
 });
